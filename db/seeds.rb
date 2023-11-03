2adbc32cb5e137adf7a8e5e1f08dc85d732bed6e
Ikenna Richard <oguejioforrichard@gmail.com>
2023-11-03 21:13:07 +0100

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,16 +9,16 @@ Cinema.delete_all
 Movie.delete_all
 CinemaMovie.delete_all
 
-cinema1 = Cinema.create(name: "Cinema 1", location: 'Lekki')
-cinema2 = Cinema.create(name: "Cinema 2", location: 'Victoria Island')
-cinema3 = Cinema.create(name: "Cinema 3", location: 'Ikeja')
+alpha = Cinema.create(name: "Cinema 1", location: 'Lekki')
+beta = Cinema.create(name: "Cinema 2", location: 'Victoria Island')
+charlie = Cinema.create(name: "Cinema 3", location: 'Ikeja')
 
-movie1 =  Movie.create(title: 'No way home', duration: '2 hrs')
-movie2 =  Movie.create(title: 'Teddy', duration: '1 hrs')
+spider_man =  Movie.create(title: 'No way home', duration: '2 hrs')
+never_land =  Movie.create(title: 'Teddys Fortune', duration: '1 hrs')
 
 viewing = CinemaMovie.create([
-  {cinema: cinema1, movie: movie1},
-  {cinema: cinema2, movie: movie2},
-  {cinema: cinema3, movie: movie2},
-  {cinema: cinema3, movie: movie1},
+  {cinema: alpha, movie: spider_man},
+  {cinema: beta, movie: never_land},
+  {cinema: beta, movie: never_land},
+  {cinema: charlie, movie: movie1},
 ])
