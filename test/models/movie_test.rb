c537a9d37b7324155f9fc96571c35eab522e93a0
Ikenna Richard <oguejioforrichard@gmail.com>
2023-11-04 19:40:59 +0100

    Add unit test for movie

--- test/models/movie_test.rb
+++ test/models/movie_test.rb
@@ -1,7 +1,12 @@
 require "test_helper"
 
 class MovieTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  fixtures :movies
+  
+  test 'movie attributes must not be empty' do
+    movie = Movie.new
+    assert movie.invalid?
+    assert movie.errors[:title].any?
+    assert movie.errors[:duration].any?
+  end
 end
