789473c74049d77e561e4a1ff3aa1f05b6563e3a
Ikenna Richard <oguejioforrichard@gmail.com>
2023-11-04 09:01:03 +0100

    Add unique contrainst to cinema name

--- app/models/cinema.rb
+++ app/models/cinema.rb
@@ -2,5 +2,6 @@ class Cinema < ApplicationRecord
   has_many :cinema_movies
   has_many :movies, through: :cinema_movies
 
-  validates :name, presence: true
+  validates :name, :location, presence: true
+  validates :name, uniqueness: { scope: :location, message: "Cinema in the same location must have a unique name" }
 end
