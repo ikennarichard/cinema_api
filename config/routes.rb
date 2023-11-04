3c2baf6c970b695c3c5e4b268eb74d4ab0aaf48f
Ikenna Richard <oguejioforrichard@gmail.com>
2023-11-04 09:05:12 +0100

    Add destroy route to cinema resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,12 @@
 Rails.application.routes.draw do
   namespace :api, :defaults => {:format => :json} do
     namespace :v1 do
-      resources :cinemas, only: [:index, :create, :show], param: :name
+      resources :cinemas, only: [:index, :create, :show, :destroy], param: :name
       resources :movies, only: [:index, :create, :show]
       get '/cinemas/:name/movies', to: 'cinemas#get_movies'
+      get '/movies/:id/cinemas', to: 'movies#get_cinemas'
     end
   end
-
-  
-  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
