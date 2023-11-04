bcf68458b17e29042491b1a4a49d562d485c2d5c
Ikenna Richard <oguejioforrichard@gmail.com>
2023-11-04 12:39:18 +0100

    Add jwt gem and user routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :users
   namespace :api, :defaults => {:format => :json} do
     namespace :v1 do
       resources :cinemas, only: [:index, :create, :show, :destroy], param: :name
