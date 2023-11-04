0a4a65383e5c3449b03441ba4d1efd70ee9cc270
Ikenna Richard <oguejioforrichard@gmail.com>
2023-11-04 19:40:41 +0100

    Add unit test for cinema

--- test/models/cinema_test.rb
+++ test/models/cinema_test.rb
@@ -1,7 +1,15 @@
 require "test_helper"
 
 class CinemaTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  fixtures :cinemas
+
+  test 'name should be unique within the same location' do
+    cinema = Cinema.new( 
+      name: cinemas(:movieland).name, 
+      location: 'Antartica'
+      )
+
+    assert_not cinema.valid?
+    assert_equal ["Cinema in the same location must have a unique name"], cinema.errors[:name]
+  end
 end
