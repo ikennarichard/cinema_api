3922d596bc8b8a7549a863600a25e1af5485dbe3
Ikenna Richard <oguejioforrichard@gmail.com>
2023-11-10 07:19:37 +0100

    Add cinema_movie test

--- test/models/cinema_movie_test.rb
+++ test/models/cinema_movie_test.rb
@@ -1,7 +1,24 @@
 require "test_helper"
 
 class CinemaMovieTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @cinema = Cinema.create(name: 'Cinema A', location: 'Aba')
+    @movie = Movie.create(title: 'Fast and Furious', duration: '3 hours')
+
+    # test associations
+    @cinema_movie = CinemaMovie.create(cinema: @cinema, movie: @movie)
+  end
+
+  test 'should be valid' do
+    assert @cinema_movie.valid?
+  end
+
+  test 'should require a cinema' do
+    @cinema_movie.cinema = nil
+    assert_not @cinema_movie.valid?
+  end
+
+  test 'should belong to a cinema' do
+    assert_equal @cinema, @cinema_movie.cinema
+  end
 end
