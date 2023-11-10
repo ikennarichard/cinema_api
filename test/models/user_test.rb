3213ce7785e99e510bc6951644d0b88454fe1bbf
Ikenna Richard <oguejioforrichard@gmail.com>
2023-11-10 07:20:31 +0100

    Add unit test for the user model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,19 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.new(
+      name: 'Riri', 
+      email: 'riri@example.com', 
+      password: 'password')
+  end
+
+  test 'should be valid' do
+    assert @user.valid?
+  end
+
+  test 'email should be present' do
+    @user.email = ''
+    assert_not @user.valid?
+  end
 end
